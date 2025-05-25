a66fd2b4ffea16441453b3d6486158a3645fd089
burhan <burhanweb111@gmail.com>
2025-05-25 17:48:34 +0530

    minor changes

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new SocketIO(server, {
   cors: {
-    origin: CLIENT_URL,
+    origin: process.env.CLIENT_URL,
     methods: ["GET", "POST"],
     credentials: true,
   },
