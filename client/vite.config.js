fdea17f72174410c4edc7f91b1ee5109fc9bee3e
burhan <burhanweb111@gmail.com>
2025-05-25 17:05:27 +0530

    change in vite.config

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "0.0.0.0", // Allow the server to be accessible externally
-    port: 5173, // Ensure the correct port is exposed
+    host: "0.0.0.0",
+    port: 5173,
+    allowedHosts: ["golive-1.onrender.com"],
   },
 });
